f2301cb9cbcb427ab3d4317e6ca5440def1557f5
choc-ayano <kondo@fusic.co.jp>
2020-08-01 14:44:53 +0900

    仮実装でテストが通った

--- src/Tddbc/ClosedRange.php
+++ src/Tddbc/ClosedRange.php
@@ -6,11 +6,25 @@ use Exception;
 
 class ClosedRange
 {
+    private int $lowerEndPoint;
+    private int $upperEndPoint;
+
+    public function __construct(int $lowerEndPoint, int $upperEndPoint)
+    {
+        $this->lowerEndPoint = $lowerEndPoint;
+        $this->upperEndPoint = $upperEndPoint;
+    }
+
     public function getFormattedClosedRange(int $lowerEndPoint, int $upperEndPoint): string
     {
-        if (!is_int($lowerEndPoint) || !is_int($upperEndPoint)) {
+        if (!(is_int($lowerEndPoint) && is_int($upperEndPoint))) {
             throw new Exception();
         }
         return "[". $lowerEndPoint.",". $upperEndPoint. "]";
+    }
+
+    public function isIncludedClosedRange(int $targetPoint): bool
+    {
+        return true;
     }
 }
--- tests/Tddbc/ClosedRangeTest.php
+++ tests/Tddbc/ClosedRangeTest.php
@@ -17,7 +17,6 @@ class ClosedRangeTest extends TestCase
      */
     protected function setUp() : void
     {
-        $this->sut = new ClosedRange();
     }
 
     /**
@@ -26,18 +25,27 @@ class ClosedRangeTest extends TestCase
      */
     public function _3_8を引数に与えれば､決められたフォーマットの文字列を返す()
     {
-        $this->assertEquals("[3,8]", $this->sut->getFormattedClosedRange(3, 8));
+        $sut = new ClosedRange(1, 4);
+        $this->assertEquals("[3,8]", $sut->getFormattedClosedRange(3, 8));
+    }
+
+    /**
+     * @test
+     * @expectedExceptionMessage not int argument
+     */
+    public function 整数以外を引数に渡すとExceptionを返す()
+    {
+        $sut = new ClosedRange(1, 4);
+        $sut->getFormattedClosedRange('3','8');
+        $this->assertTrue(true);
     }
 
     /**
      * @test
      */
-    public function 整数以外を引数に渡すとExceptionを返す()
+    public function 引数で渡した整数が下端点よりも大きく､上端点よりも小さいことが判定できる()
     {
-        try {
-            $this->sut->getFormattedClosedRange('3','8');
-        } catch (Exception $e) {
-            $this->assertTrue(true);
-        }
+        $sut = new ClosedRange(1, 4);
+        $this->assertTrue($sut->isIncludedClosedRange(3));
     }
 }
